eebff4c712679402f96c05a142162d4179382c9c
Bajjuri Sai Teja <bsaiteja1996@gmail.com>
2018-08-09 12:45:38 +0530

    parent to subchild

--- src/app/task4/task4.module.ts
+++ src/app/task4/task4.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from "@angular/platform-browser";
-
 import { Child1Component } from './child1/child1.component';
 import { Child2Component } from './child2/child2.component';
 import { Child3Component } from './child3/child3.component';
@@ -9,7 +7,6 @@ import { Child3Component } from './child3/child3.component';
 @NgModule({
   imports: [
     CommonModule,
-    BrowserModule
   ],
   declarations: [Child1Component, Child2Component, Child3Component],
   exports:[Child1Component, Child2Component, Child3Component]
